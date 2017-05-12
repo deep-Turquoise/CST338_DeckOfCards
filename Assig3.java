a1d743305ce59cc617b490b70e65f0621978e8de
deep-Turquoise <nicholasnelson10@gmail.com>
2017-05-12 15:43:00 -0700

    More basic setup

--- Assig3.java
+++ Assig3.java
@@ -1,5 +1,26 @@
+// Nicholas Nelson, David Henderson, Christopher Calderon
+// Nathan Mauga, Hayley Dimapillis
+// CST338-30_SU17: Software Design
+// Module 3
+// Write a Java program: Decks of Cards (4 hrs)
 
 public class Assig3
 {
 
 }
+
+public class Card
+{
+
+}
+
+public class Deck
+{
+
+}
+
+public class Hands
+{
+
+}
+
