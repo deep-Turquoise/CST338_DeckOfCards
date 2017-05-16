40a2e7e5a9d153f429864b31a729109eb3b6847b
deep-Turquoise <nicholasnelson10@gmail.com>
2017-05-15 17:41:19 -0700

    cleaned up formatting developed main method - fixed classes error

--- Assig3.java
+++ Assig3.java
@@ -6,44 +6,55 @@
 
 public class Assig3
 {
-
+   public static void main(String[] args)
+   {
+      // class Card test
+      Card legalCard1 = new Card('2', Card.Suit.diamonds);
+      Card legalCard2 = new Card('5', Card.Suit.hearts);
+      Card illegalCard1 = new Card('Z', Card.Suit.hearts);
+      System.out.println("----------------------------------------------------");
+      System.out.println(legalCard1.toString());
+      System.out.println(legalCard2.toString());
+      System.out.println(illegalCard1.toString());
+      System.out.println("----------------------------------------------------");
+   }
 }
 
-public class Card
+class Card
 {
    public enum Suit {clubs, diamonds, hearts, spades}
    public static char[] values = {'A','2','3','4','5','6','7','8','9','T','J','Q','K'};
    private char value;
    private Suit suit;
    private boolean errorFlag;
-   
+
    Card()
    {
       value = 'A';
       suit = Suit.spades;
       errorFlag = false;
    }
-   
+
    Card(char value, Suit suit)
    {
       set(value, suit); 
    }
-   
+
    public char getValue()
    {
       return value;
    }
-   
+
    public Suit getSuit()
    {
       return suit;
    }
-   
+
    public boolean getErrorFlag()
    {
       return errorFlag;
    }
-   
+
    public boolean set(char newValue, Suit newSuit)
    {
       if (isValid(newValue, newSuit) == true)
@@ -53,7 +64,6 @@ public class Card
          errorFlag = false;
          return true;
       }
-      
       else
       {
          errorFlag = true;
@@ -61,7 +71,7 @@ public class Card
          return false;
       }     
    }
-   
+
    public String toString()
    {
       if (errorFlag == true)
@@ -70,7 +80,7 @@ public class Card
       }
       return getValue() + " of " + getSuit();
    }
-   
+
    private boolean isValid(char value, Suit suit)
    {
       for (int i = 0; i < values.length; i++)
@@ -84,18 +94,18 @@ public class Card
    }
 }
 
-public class Hand
+class Hand
 {
    public static int MAX_CARDS = 50;
    private Card[] myCards;
    private int numCards;
-   
+
    Hand()
    {
       numCards = 0;
       myCards = new Card[MAX_CARDS];
    }
-   
+
    public void resetHand()
    {
       for (int i = 0; i < numCards; i++)
@@ -104,7 +114,7 @@ public class Hand
          numCards = 0;
       }
    }
-   
+
    public boolean takeCard(Card card)
    {
       if (numCards != MAX_CARDS)
@@ -115,37 +125,37 @@ public class Hand
       }
       return false;
    }
-   
+
    public Card playCard()
    {
       return myCards[numCards--];
    }
-   
+
    public String toString()
    {
       String hand = "Hand:\n";
-      
+
       for (int i = 0; i < numCards; i++)
       {
          hand += myCards[i].toString() + "\n";
       }
-      
+
       return hand;
    }
-   
+
    public int getNumCards()
    {
       return numCards;
    }
-   
-   public Card inspectCard(int k)
-   {
-      //I am confused about this one - Christopher
-   }
+
+   //    public Card inspectCard(int k)
+   //    {
+   //       //I am confused about this one - Christopher
+   //    }
 }
 
-public class Deck
+class Deck
 {
-   
+
 }
 
